d6df752a33e791c5e54685739cfa4899373fb825
Christian <c.stamariab@gmail.com>
2017-01-23 11:58:26 -0300

    primer commit del versionamiento del curso

---
+++ server/publications.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Christian on 22-01-2017.
+ */
+import { Meteor } from 'meteor/meteor';
+import { Projects } from '../lib/collections/projects';
+
+Meteor.publish('projects', function () {
+   return Projects.find({owner: this.userId});
+});
