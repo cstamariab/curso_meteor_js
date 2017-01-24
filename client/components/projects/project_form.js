a431f81ae08073c556e243b0f66de0fc6df2725a
Christian <c.stamariab@gmail.com>
2017-01-23 23:31:23 -0300

    Metodos para eliminar proyectos y Desarrollo del Project Detail

--- client/components/projects/project_form.js
+++ client/components/projects/project_form.js
@@ -2,9 +2,19 @@
  * Created by Christian on 23-01-2017.
  */
 import { Projects } from '../../../lib/collections/projects';
-
+import { AutoForm } from 'meteor/aldeed:autoform';
+import { Router }  from 'meteor/iron:router';
 Template.projectForm.helpers({
     formCollection() {
        return Projects;
     }
 });
+
+Template.projectForm.onCreated(function () {
+    AutoForm.addHooks(['projectForm'],{
+        onSuccess: function (operation,result,template) {
+            Router.go('/projects');
+        }
+    });
+});
+
---
+++ client/components/projects/projects.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Christian on 23-01-2017.
+ */
+import { Meteor } from 'meteor/meteor';
+
+Template.projects.events({
+    'click .remove': function (event, template) {
+       Meteor.call('projects.remove' , this._id);
+    }
+})
